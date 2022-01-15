724d3643d705af8a6dde059f5de92ecb8c272263
noordseoostende <voropayev1@mail.ru>
2022-01-15 22:32:11 +0100

    latitude

---
+++ src/HemisphereDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HemisphereDisplay = () => {
+  return (
+    <div>
+      Hey you are in Nothern hemisphere!!
+    </div>
+  )
+}
+
+export default HemisphereDisplay
